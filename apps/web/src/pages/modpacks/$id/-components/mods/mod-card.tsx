54b216a81afa6fd2e14338fdecfcc51e67056859
Matheus Agostinho <contact@matheuspa.com>
2025-12-14 10:58:51 -0300

    feat(mods): refactor ModCard layout for improved structure and styling

--- apps/web/src/pages/modpacks/$id/-components/mods/mod-card.tsx
+++ apps/web/src/pages/modpacks/$id/-components/mods/mod-card.tsx
@@ -23,50 +23,48 @@ export function ModCard({ data, modpackId, canManage }: ModCardProps) {
   console.log(data)
 
   return (
-    <Card className="flex flex-row items-center justify-between p-0 gap-4 overflow-clip">
-      <div className="flex flex-row items-center gap-4">
-        <div className="relative bg-primary/30 dark:bg-primary aspect-square h-24 flex items-center justify-center  text-muted-foreground/20 overflow-clip">
-          {data.avatarUrl ? (
-            <img
-              src={data.avatarUrl}
-              alt={data.name}
-              className="w-full h-full object-cover"
-            />
-          ) : (
-            <img
-              src={
-                theme === 'light'
-                  ? '/brand/zumbi-danced.svg'
-                  : '/brand/zumbi-danced-dark.svg'
-              }
-              alt={data.name}
-              className="h-8 opacity-80"
-            />
-          )}
-        </div>
-        <div className="flex flex-col">
-          <CardTitle className="text-base hover:underline">
-            <Link
-              to={data.steamUrl || '#'}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {data.name}
-            </Link>
-          </CardTitle>
-          <CardDescription className="line-clamp-2">
-            {data.description}
-          </CardDescription>
-        </div>
+    <Card className="flex flex-row items-center p-0 overflow-hidden gap-0">
+      <div className="relative bg-primary/30 dark:bg-primary aspect-square h-full flex items-center justify-center  text-muted-foreground/20 overflow-clip">
+        {data.avatarUrl ? (
+          <img
+            src={data.avatarUrl}
+            alt={data.name}
+            className="w-full h-full object-cover"
+          />
+        ) : (
+          <img
+            src={
+              theme === 'light'
+                ? '/brand/zumbi-danced.svg'
+                : '/brand/zumbi-danced-dark.svg'
+            }
+            alt={data.name}
+            className="h-8 opacity-80"
+          />
+        )}
       </div>
+      <div className="p-2">
+        <Link
+          to={data.steamUrl || '#'}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h1 className="text-base hover:underline wrap-break-word">
+            {data.name}
+          </h1>
+        </Link>
+        <p className="line-clamp-2 text-sm text-muted-foreground">
+          {data.description}
+        </p>
 
-      {canManage && (
-        <RemoveModDialog
-          modpackId={modpackId}
-          modId={data.id}
-          modName={data.name}
-        />
-      )}
+        {canManage && (
+          <RemoveModDialog
+            modpackId={modpackId}
+            modId={data.id}
+            modName={data.name}
+          />
+        )}
+      </div>
     </Card>
   )
 }
