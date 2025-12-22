182b3fa808eb463bedce411671bb8771ec730817
Matheus Agostinho <contact@matheuspa.com>
2025-12-22 20:47:58 -0300

    fix: add maxRetries option to RedisClient initialization

--- packages/cache/index.ts
+++ packages/cache/index.ts
@@ -1,4 +1,6 @@
 import { RedisClient } from 'bun'
 import { env } from './env'
 
-export const cacheClient = new RedisClient(env.SECONDARY_DATABASE_URL)
+export const cacheClient = new RedisClient(env.SECONDARY_DATABASE_URL, {
+  maxRetries: 20,
+})
