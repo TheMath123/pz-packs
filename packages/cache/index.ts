567c205467cc8a611f7d6aca659950bc8d072509
Matheus Agostinho <contact@matheuspa.com>
2025-12-23 17:15:59 -0300

    fix: enhance Redis client setup with error handling and connection duplication

--- packages/cache/index.ts
+++ packages/cache/index.ts
@@ -1,4 +1,19 @@
 import Redis from 'ioredis'
 import { env } from './env'
 
-export const cacheClient = new Redis(env.SECONDARY_DATABASE_URL)
+console.log(env.SECONDARY_DATABASE_URL)
+export const redisClient = new Redis(env.SECONDARY_DATABASE_URL, {
+  maxRetriesPerRequest: 20,
+})
+
+export const bullmqConnection = redisClient.duplicate({
+  maxRetriesPerRequest: null,
+})
+
+redisClient.on('error', (err) => {
+  console.error('Redis Connection Error:', err)
+})
+
+bullmqConnection.on('error', (err) => {
+  console.error('BullMQ Client Error:', err)
+})
