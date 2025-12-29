c8eac374ea9ecee811fb6800c3d7468a610a3b14
Matheus Agostinho <contact@matheuspa.com>
2025-12-29 13:08:16 -0300

    feat: add plugins for admin client and additional user fields in auth client

--- packages/auth/client.ts
+++ packages/auth/client.ts
@@ -1,8 +1,26 @@
+import { adminClient, inferAdditionalFields } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react'
+import { ac, admin, user } from './permissions'
 
 export function generateAuthClient(baseURL: string) {
   return createAuthClient({
     baseURL: baseURL,
     basePath: '/auth',
+    plugins: [
+      adminClient({
+        ac,
+        roles: {
+          admin,
+          user,
+        },
+      }),
+      inferAdditionalFields({
+        user: {
+          role: {
+            type: 'string',
+          },
+        },
+      }),
+    ],
   })
 }
